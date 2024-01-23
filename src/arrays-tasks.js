61f1a996e89a4bff34a58d30de9a48df20aa0348
Sergei Shevchenko <wunder49@yandex.by>
2024-01-23 21:23:40 +0300

    add solution for isValueEqualsIndex

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -171,8 +171,9 @@ function isSameLength(arr) {
  *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
  *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
  */
-function isValueEqualsIndex(/* arr */) {
-  throw new Error('Not implemented');
+function isValueEqualsIndex(arr) {
+  // throw new Error('Not implemented');
+  return arr.some((item, index) => item === index);
 }
 
 /**
