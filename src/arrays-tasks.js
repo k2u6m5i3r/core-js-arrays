3b5d8270a0de13e27e8f221618e62ddc18196364
Sergei Shevchenko <wunder49@yandex.by>
2024-01-23 21:46:55 +0300

    add solution for getIndicesOfOddNumbers

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -453,8 +453,14 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  // throw new Error('Not implemented');
+  return numbers.reduce((acc, item, index) => {
+    if (item % 2 === 1) {
+      acc.push(index);
+    }
+    return acc;
+  }, []);
 }
 
 /**
